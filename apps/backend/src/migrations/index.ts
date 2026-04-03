f48dd6487cc8655452401f110a51544196897d9a
Ares Shrestha <xtutorials66@gmail.com>
2026-04-03 13:16:58 +0545

    migration: created

--- apps/backend/src/migrations/index.ts
+++ apps/backend/src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20260205_060627 from './20260205_060627';
 import * as migration_20260210_024142 from './20260210_024142';
 import * as migration_20260212_084929 from './20260212_084929';
 import * as migration_20260321_170629 from './20260321_170629';
+import * as migration_20260403_073038 from './20260403_073038';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20260321_170629.up,
     down: migration_20260321_170629.down,
-    name: '20260321_170629'
+    name: '20260321_170629',
+  },
+  {
+    up: migration_20260403_073038.up,
+    down: migration_20260403_073038.down,
+    name: '20260403_073038'
   },
 ];
